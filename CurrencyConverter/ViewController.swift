007304d27264e73735e1e4303c1d0877e45ab463
rricamar <r.ricamarti@gmail.com>
2022-03-21 11:49:13 +0100

    Add Currency Converter

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rateTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var currencySegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        label.text = "Currency Converter";
     }
 
-
+    @IBAction func convertTapped(_ sender: Any) {
+        let rate = Double(rateTextField.text!)!;
+        let amount = Double(amountTextField.text!)!;
+        let converted = rate * amount;
+        
+        if currencySegment.selectedSegmentIndex == 0 {
+            label.text = "$\(converted)";
+        } else {
+            label.text = "¥\(converted)";
+        }
+    }
+    
 }
 
